4200682d8446e48c69a7cf8a1bf1a5d3c3c385fd
Oscar Lin <linoscar01@gmail.com>
2023-06-28 17:54:59 -0400

    add and delete letters

--- app/context/WordlyContext.tsx
+++ app/context/WordlyContext.tsx
@@ -24,25 +24,43 @@ export const WordlyContextProvider = ({
       });
   }, []);
 
-  useEffect(()=>{
-    document.addEventListener("keyup", keyHandler)
+  useEffect(() => {
+    document.addEventListener("keyup", keyHandler);
 
-    return ()=>{
-      document.removeEventListener("keyup", keyHandler)
+    return () => {
+      document.removeEventListener("keyup", keyHandler);
+    };
+  }, [keyHandler]);
+
+  function keyHandler(event: KeyboardEvent) {
+    if (event.key === "Backspace") {
+      deleteLetter();
+    } else if (event.key === "Enter") {
+      console.log("enter");
+    } else if(/^[A-Za-z]$/.test(event.key)){
+      addLetter(event.key)
+    }else {
+      console.log("invalid keystroke", event.key)
     }
-  }, [keyHandler])
-
-  function keyHandler(event: KeyboardEvent){
-    console.log(event.key)
-    setCurrentPosition(currentPosition=> currentPosition+1)
-    console.log(currentTurn, currentPosition)
-    updateBoard(event.key);
   }
 
-  function updateBoard(letter: string){
-    const tempBoard = board.map(row => [...row]);
-    tempBoard[currentTurn][currentPosition] = letter;
-    setBoard(tempBoard)
+  function updateBoard(letter: string, turn: number, position: number) {
+    // console.log("updating", turn, position);
+    const tempBoard = board.map((row) => [...row]);
+    tempBoard[turn][position] = letter;
+    setBoard(tempBoard);
+  }
+
+  function deleteLetter() {
+    if (currentPosition === 0) return;
+    updateBoard("", currentTurn, currentPosition - 1);
+    setCurrentPosition((currentPosition) => currentPosition - 1);
+  }
+
+  function addLetter(key: string) {
+    if (currentPosition === 5) return;
+    updateBoard(key, currentTurn, currentPosition);
+    setCurrentPosition((currentPosition) => currentPosition + 1);
   }
 
   const [word, setWord] = useState("");
@@ -54,8 +72,8 @@ export const WordlyContextProvider = ({
     ["", "", "", "", ""],
     ["", "", "", "", ""],
   ]);
-  const [currentTurn, setCurrentTurn] = useState(0)
-  const [currentPosition, setCurrentPosition] = useState(0)
+  const [currentTurn, setCurrentTurn] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState(0);
 
   return (
     <WordlyContext.Provider value={{ word, board }}>
