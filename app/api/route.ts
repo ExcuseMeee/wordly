106749a55442a71204d09f87c3ded24af92cd2bd
Oscar Lin <linoscar01@gmail.com>
2023-07-10 17:53:31 -0400

    expanded allowed guesses

--- app/api/route.ts
+++ app/api/route.ts
@@ -15,9 +15,13 @@ export async function GET() {
 export async function POST(request: Request) {
   try {
     const words = await fs.readFile("./word-bank.txt", "utf-8");
+    const additional = await fs.readFile("./allowed-guesses.txt", "utf-8");
+
     const list = words.split("\n").map((word) => word.trim());
+    const additionalList = additional.split("\n").map((word) => word.trim());
+
     const guess: string = (await request.text()).toLowerCase().trim();
-    const wordExists = list.includes(guess);
+    const wordExists = list.includes(guess) || additionalList.includes(guess);
     return NextResponse.json(wordExists, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Word search failed" }, { status: 500 });
