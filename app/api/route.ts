459a9cf559e7d954c3a9dd7461ab12b71ec1b9ea
Oscar Lin <linoscar01@gmail.com>
2023-07-16 16:12:42 -0400

    test

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const words = await fs.readFile("./word-bank.txt", "utf-8");
+    const words = await fs.readFile("word-bank.txt", "utf-8");
     const list = words.split("\n").map((word) => word.trim());
     const randIndex = Math.floor(Math.random() * list.length);
     return NextResponse.json(list.at(randIndex), { status: 200 });
@@ -14,8 +14,8 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
-    const words = await fs.readFile("./word-bank.txt", "utf-8");
-    const additional = await fs.readFile("./allowed-guesses.txt", "utf-8");
+    const words = await fs.readFile("word-bank.txt", "utf-8");
+    const additional = await fs.readFile("allowed-guesses.txt", "utf-8");
 
     const list = words.split("\n").map((word) => word.trim());
     const additionalList = additional.split("\n").map((word) => word.trim());
